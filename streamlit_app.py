2e84ebb04d7dd6ccfded0ca1be18b369199c8b77
vaibhavsnowflake <127179639+vaibhavsnowflake@users.noreply.github.com>
2023-03-07 11:56:50 +0530

    Update streamlit_app.py
    imported pandas library and read a csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,8 @@ streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado-toast')
 streamlit.text('🍌🥭 Build your own Shake🥝🍇')
+
+
+import pandas
+my_fruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list.dataframe()
