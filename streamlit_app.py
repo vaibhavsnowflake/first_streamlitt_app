49086cec308d01ac4f24ddeb2f6709fb32651ca4
vaibhavsnowflake <127179639+vaibhavsnowflake@users.noreply.github.com>
2023-03-07 11:52:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit
 
 streamlit.title('My Parents Healthy Diner') 
 streamlit.header('Breakfast Menu')
-streamlit.text('Pan Cakes')
-streamlit.text('coffee')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
