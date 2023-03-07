bcb7cb291147d1aba2aa865e115597e065528c54
vaibhavsnowflake <127179639+vaibhavsnowflake@users.noreply.github.com>
2023-03-07 11:57:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ streamlit.text('🍌🥭 Build your own Shake🥝🍇')
 
 import pandas as pd
 my_fruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list.dataframe()
+streamlit.dataframe(my_fruit_list)
