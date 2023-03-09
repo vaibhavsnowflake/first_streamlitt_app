40d4e83716bf415a0d3aef4c66c8e29b0c15f3d5
vaibhavsnowflake <127179639+vaibhavsnowflake@users.noreply.github.com>
2023-03-09 17:50:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("This Fruit list contains")
-streamlit.text(my_data_row)
+streamlit.text(my_data_row) 
 
