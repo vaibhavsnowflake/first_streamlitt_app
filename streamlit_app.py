763f3b6df11824e02d0388a9013de57962d9c040
vaibhavsnowflake <127179639+vaibhavsnowflake@users.noreply.github.com>
2023-03-10 10:05:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
+
+
+#import snowflake connector and the libraries
 import streamlit
-#import snowflake connector
 import snowflake.connector
+import pandas as pd
+import requests
+#for error handling
+from urllib.erroe import URLError 
 
 streamlit.title('My Parents Healthy Diner') 
 streamlit.header('Breakfast Menu')
@@ -11,7 +17,6 @@ streamlit.text('🥑🍞Avacado-toast')
 streamlit.text('🍌🥭 Build your own Shake🥝🍇')
 
 
-import pandas as pd
 
 my_fruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -27,9 +32,11 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+#won't run anything past this
+streamlit.stop()
 
 
 # write your own comment -what does the next line do? 
